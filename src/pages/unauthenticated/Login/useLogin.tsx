582e52a1d09029b6052e7843105d5066dcd8b2e1
Jokūbas Toločka <jokubas.tj@gmail.com>
2023-04-26 16:29:27 +0100

    remove unused var

--- src/pages/unauthenticated/Login/useLogin.tsx
+++ src/pages/unauthenticated/Login/useLogin.tsx
@@ -43,7 +43,7 @@ const useLogin = () => {
 
     checkIfUserExists({
       variables: { walletAddress },
-      onCompleted: async ({ checkIfUserExists: { walletAddress, nonce, hasSkippedEmail, email } }) => {
+      onCompleted: async ({ checkIfUserExists: { walletAddress, nonce, hasSkippedEmail } }) => {
         // // signing the received message via metamask
         const formattedMessage = `Nonce for this message: ${nonce}`;
         const signature = await signMessageAsync({ message: formattedMessage });
